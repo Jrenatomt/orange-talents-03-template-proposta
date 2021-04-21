3d010a6c008830de0143e5532efcec14268945d0
Jrenatomt <jrenatomt@gmail.com>
2021-04-21 10:17:25 -0300

    adicionando o keycloak

--- proposta/src/test/java/com/renato/proposta/proposta/PropostaControllerTest.java
+++ proposta/src/test/java/com/renato/proposta/proposta/PropostaControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +21,7 @@ import com.renato.proposta.cadastraProposta.PropostaRequest;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@ActiveProfiles("test")
 public class PropostaControllerTest {
 	
 	@Autowired
