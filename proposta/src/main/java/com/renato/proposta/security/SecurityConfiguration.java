f970b0ead5a030a5ab1f6914b4811fe09d08fc64
Jrenatomt <jrenatomt@gmail.com>
2021-04-26 10:13:46 -0300

    prometheus

--- proposta/src/main/java/com/renato/proposta/security/SecurityConfiguration.java
+++ proposta/src/main/java/com/renato/proposta/security/SecurityConfiguration.java
@@ -12,21 +12,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
-//	@Override
-//	public void configure(WebSecurity web) throws Exception {
-//		web.ignoring().antMatchers("/**");
-//	}
-//	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests(authorizeRequests -> authorizeRequests
-				.antMatchers(HttpMethod.GET, "/api/propostas/**").hasAuthority("SCOPE_propostas:read")
-				.antMatchers(HttpMethod.GET, "/api/cartoes/**").hasAuthority("SCOPE_cartoes:read")
-				.antMatchers(HttpMethod.POST, "/api/cartoes/**").hasAuthority("SCOPE_cartoes:write")
-				.antMatchers(HttpMethod.POST, "/api/propostas/**").hasAuthority("SCOPE_propostas:write")
-				.anyRequest().authenticated())
-				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-		        .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/**");
 	}
+	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests(authorizeRequests -> authorizeRequests
+//				.antMatchers(HttpMethod.GET, "/api/propostas/**").hasAuthority("SCOPE_propostas:read")
+//				.antMatchers(HttpMethod.GET, "/api/cartoes/**").hasAuthority("SCOPE_cartoes:read")
+//				.antMatchers(HttpMethod.POST, "/api/cartoes/**").hasAuthority("SCOPE_cartoes:write")
+//				.antMatchers(HttpMethod.POST, "/api/propostas/**").hasAuthority("SCOPE_propostas:write")
+//				.anyRequest().authenticated())
+//				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+//		        .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//	}
 }
